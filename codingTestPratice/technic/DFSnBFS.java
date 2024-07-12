0135f0694732db8d917df9d2d91cfe3e85e39d38
ysh195 <159318873+ysh195@users.noreply.github.com>
2024-07-12 17:38:39 +0900

    Update DFSnBFS.java

--- codingTestPratice/technic/DFSnBFS.java
+++ codingTestPratice/technic/DFSnBFS.java
@@ -23,7 +23,7 @@ import java.util.Scanner;
  * 
  */
 
-// 원리는 잘 이해가 안 되지만 꽤 간결한데?
+// 꽤 간결한데?
 
 public class DFSnBFS {
 	
@@ -61,29 +61,32 @@ public class DFSnBFS {
 		System.out.println();
 	}
 	
-	public static void DFS(int Node) {
+	public static void DFS(int Node) { // 던전 탐색에서 길 하나를 끝까지 탐색하고 돌아 나오는 방식
 		System.out.println(Node + " ");
 		visited[Node] = true;
-		for(int i : A[Node]) {
-			if(!visited[i]) {
+		for(int i : A[Node]) { // i 자체가 A의 요소고, 그 요소는 숫자니까 계속 파고 들어감.
+			if(!visited[i]) { // visited가 false, 그러니까 아직 안 와본 곳이면 들어감.
 				DFS(i);
 			}
-		}
-	}
+		} // 와본 곳이면 그냥 지나감.
+	} // 이거 자체가 내부에 무수히 많은 for문이 있는 것이나 마찬가지니까 들어갈 곳이 없으면 다 지나가는 거고, 해당 for문이 끝나는 거임.
+	// 그리고 내부의 for문이 끝나면 밖에 있는 for문이 이어서 진행될 뿐.
+	// 자기 자신을 내부에 담고 있는데다, for문을 무수히 내부에 반복하니까 거부감이 있긴 한데 확실히 쉽고 간결하다.
 	
-	public static void BFS(int Node) {
+	public static void BFS(int Node) { // 던전 탐색에서 초반-중반-종반 등 앞에서 뒤로 각 구간을 완벽히 탐색하는 방식
 		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.add(Node);
-		visited[Node] = true;
+		queue.add(Node); // 큐에 추가하고
+		visited[Node] = true; // 왔다고 표시
 		
-		while(!queue.isEmpty()) {
-			int now_Node = queue.poll();
+		while(!queue.isEmpty()) { // 큐에 내용물이 없어질 때까지 반복
+			int now_Node = queue.poll(); // 큐는 poll 하면 앞에서부터 제거 및 출력
 			System.out.println(now_Node + " ");
-			for(int i : A[now_Node]) {
-				if(!visited[i]) {
-					queue.add(i);
+			for(int i : A[now_Node]) { // 그리고 출력된 것을 A에 집어넣어서 그 요소를 살펴봄
+				if(!visited[i]) { // 안 가본 곳이면
+					visited[i] = true; // 왔다고 표시
+					queue.add(i); // 그리고 큐에 추가.
 				}
-			}
+			} // 내용물이 다 떨어질 때까지 안 가본 곳을 집어넣고 다시 출력하기를 반복
 		}
 	}
 
